e0cab501e9619c5275ff35caf895f5fa53806f86
Br3-kyokyo <danibusters.main@gmail.com>
2017-07-15 23:45:07 +0900

    今までに開発したものをGithubにup
    main.java…いわずもがな
    Q_code.java…Q也が書いたやつ。CSVを転置する。

---
+++ src/Q_code.java
@@ -0,0 +1,117 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Q_code {
+
+	int num_cols;// ファイルの列の要素の数
+	int num_rows;// ファイルの行の要素の数
+	List<String> list_file = new LinkedList<String>();// ファイルの行のデータを詰めたリスト
+
+	String[][] data;// ２次元配列
+
+	/**
+	 * ファイルを読み込んで行をString型でlist_fileに詰め込む
+	 *
+	 * @param path
+	 *            読み込むファイルのパス
+	 * @throws IOException
+	 * @throws FileNotFoundException
+	 */
+	private void read_file(String path) throws IOException {
+
+		InputStreamReader filereader = new InputStreamReader(new FileInputStream(new File(path)),"Shift_JIS");
+
+		BufferedReader br = new BufferedReader(filereader);
+
+		String str;
+
+		while ((str = br.readLine()) != null) {
+			list_file.add(str);
+		}
+
+		br.close();
+	}
+
+	/**
+	 * リストからStringの2次元配列に変換する
+	 *
+	 * @throws IOException
+	 */
+	private void CreateMat() {
+		num_rows = list_file.size();
+		String[] sv_buf = list_file.get(0).split(",", -1);
+		num_cols = sv_buf.length;
+
+		data = new String[num_rows][num_cols];
+
+		for (int i = 0; i < num_rows; i++) {
+			sv_buf = list_file.get(i).split(",", -1);
+
+			for (int j = 0; j < num_cols; j++) {
+					try{
+					data[i][j] = sv_buf[j];
+				}catch(ArrayIndexOutOfBoundsException e){
+					continue;
+				}
+			}
+		}
+	}
+
+	/**
+	 * ファイルに転置したものを書き出す
+	 *
+	 * @param path
+	 * @throws IOException
+	 */
+	private void Write(String path) throws IOException {
+		File file = new File(path);
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file),"Shift_JIS"));
+
+		for (int i = 0; i < num_cols; i++) {
+			for (int j = 0; j < num_rows; j++) {
+				if(data[j][i]!=null){
+					bw.write(data[j][i] + ",");
+				}else{
+					bw.write(",");
+				}
+			}
+			bw.newLine();
+		}
+
+		bw.close();
+	}
+
+	Q_code(String src, String dst ) throws IOException {
+		read_file(src);// !適宜変更
+		Row_deform(); //行の入れ替え
+		CreateMat();
+		Write(dst);// !適宜変更
+	}
+
+	private void Row_deform() {
+		list_file.add(2, list_file.get(26 - 6));
+		list_file.remove(27 - 6);
+
+		list_file.add(3, list_file.get(27 - 6));
+		list_file.remove(28 - 6);
+
+		list_file.add(4, list_file.get(40 - 6));
+		list_file.remove(41 - 6);
+
+		list_file.add(0, list_file.get(77 - 6));
+		list_file.remove(78- 6);
+
+
+	}
+
+}
---
+++ src/main.java
@@ -0,0 +1,410 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+class main {
+
+	private static PrintWriter pw;
+	private static Iterator<String> fieldNames_g;
+	private static Iterator<String> fieldNames_e;
+	private static JsonNode root_a;
+	private static JsonNode root_g;
+	private static JsonNode root_e;
+	private static HashMap<String, Integer> id_row;
+	private static BufferedWriter bw;
+	private static File file;
+
+
+	public static void main(String[] args) throws JsonProcessingException, IOException{
+
+		String csv_location = null;
+		String jsn_location = null;
+
+		try{
+			csv_location = args[0];
+			jsn_location = args[1];
+		}catch(ArrayIndexOutOfBoundsException e){
+
+			System.out.println("第一引数:CSVを出力したいディレクトリのパス ex) ../home/Desktop  ");
+			System.out.println("第二引数:jsonファイルの存在するディレクトリのパス ex) ../deta/sohosai17  ");
+			System.out.println("を指定してください");
+
+			System.exit(0);
+
+		}
+
+
+		System.out.println("Start");
+
+		id_row = new HashMap<String,Integer>();	//団体IDと列が入っている
+
+		file = new File(csv_location + "/temp.csv"); //ファイルを作成
+
+		if(file.exists()){		//既にファイルがあれば再生成する。
+			file.delete();
+			file.createNewFile();
+		}else{
+			file.createNewFile();
+		}
+
+		bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file),"Shift_JIS"));
+		pw = new PrintWriter(bw);
+
+		ObjectMapper mapper = new ObjectMapper();
+		root_a = mapper.readTree(new File(jsn_location + "/account.json"));
+		root_g = mapper.readTree(new File(jsn_location + "/group.json"));
+		root_e = mapper.readTree(new File(jsn_location + "/event.json"));
+
+	    root_a.fieldNames();
+
+		//group.jsonを書き出し
+
+		group();			//企画団体名
+		group2("kana");	//かな
+		//group1();
+
+
+		//account.jsonを書き出し
+
+		//pw.println("以下、責任者・副責任者情報");
+
+		//アカウント情報を書き出し
+
+		account("rep");//責任者情報を書き出し
+		account("member");//副責を書き出し
+
+		//申請項目を書き出し
+		//pw.println("以下、申請情報");
+
+		event();
+
+		pw.close();
+
+
+		new Q_code(csv_location + "/temp.csv",csv_location + "/data.csv");
+	}
+
+
+
+private static void group1() throws IOException {
+
+	pw.print("団体ID,");
+
+	fieldNames_g = root_g.fieldNames();
+
+	while(fieldNames_g.hasNext()){
+		//System.out.println(fieldNames_g.next());
+		try{
+			pw.print(fieldNames_g.next()+",");
+		}catch(NoSuchElementException e){}
+	}
+	bw.newLine();
+}
+
+
+	private static void event() throws IOException {
+
+		fieldNames_e = root_e.get("form").fieldNames();
+
+
+		//凡例はfieldNamesに格納
+
+		while(fieldNames_e.hasNext()){
+
+
+			String item = fieldNames_e.next();
+			String type = null;
+
+
+			try{
+				type = root_e.get("form").get(item).get("type").toString();
+			}catch(NullPointerException e){
+
+			}
+
+			try{
+				pw.print(root_e.get("form").get(item).get("name").toString()+","); //項目名を出力
+
+			}catch(NullPointerException e){
+				continue;
+			}
+
+
+
+			int hoge1 = 1;
+		    fieldNames_g = root_g.fieldNames();
+
+
+		    String group_id;
+
+
+
+
+
+			while(fieldNames_g.hasNext()){
+
+				group_id = fieldNames_g.next();	//団体IDを走査
+
+
+				while(id_row.get(group_id) > hoge1){pw.print(","); hoge1++;}	//列をそろえる
+
+				if(type.equals("\"select\"") || type.equals("\"multiple\"")){
+
+
+						try{
+							String ans_id = root_e.get("form").get(item).get("ans").get(group_id).asText();
+
+							String temp = root_e.get("form").get(item).get("select").get(ans_id).toString();
+							temp = remove_comma(temp);
+
+							pw.print(temp+",");	//回答内容を出力
+						}catch(NullPointerException e){
+							pw.print(",");
+						}
+
+				}if(type.equals("\"text\"") || type.equals("\"textarea\"")){
+
+						try{
+							String temp = root_e.get("form").get(item).get("ans").get(group_id).toString();
+							temp = remove_comma(temp);
+							pw.print(temp+",");	//回答内容を出力
+						}catch(NullPointerException e){
+							pw.print(",");
+						}
+
+				}if(type.equals("\"order\"")){
+
+					Iterator<String> fieldName_odr = null;
+
+					try{
+						fieldName_odr = root_e.get("form").get(item).get("ans").get(group_id).fieldNames();
+
+						//各項目に関する解答をIteratorに代入
+						while(fieldName_odr.hasNext()){		//各団体が申請した物品のIDを列挙
+
+							String odr_ans = fieldName_odr.next();
+
+							try{
+								String target = root_e.get("form").get(item).get("select").get(odr_ans).toString();//物品の名前を出力
+								String temp = root_e.get("form").get(item).get("ans").get(group_id).get(odr_ans).toString();	//物品の数量を出力
+								temp = remove_comma(temp);
+								pw.print(target+":"+temp+" ");	//回答内容を出力
+							}catch(NullPointerException e){}
+						}
+
+						pw.print(",");
+
+					}catch(NullPointerException e){pw.print(",");}
+
+				}else{}
+
+				hoge1++;
+			}
+
+			bw.newLine();
+
+		}
+
+
+
+	}
+
+
+
+	private static void account(String string) throws IOException {
+
+		String syurui = string;
+
+	    account_sub("name",syurui);
+	    account_sub("kana",syurui);
+	    //account_sub("email",syurui);
+	    //account_sub("phone",syurui);
+	    //account_sub("id",syurui);
+	    account_sub("college",syurui);
+
+	}
+
+
+
+	private static void account_sub(String string,String syurui) throws IOException {
+
+	    root_a.fieldNames();
+	    fieldNames_g = root_g.fieldNames();
+
+
+	    int hoge1 = 1;
+
+
+	    output_hanrei(string,syurui);
+
+		while(fieldNames_g.hasNext()){
+
+			String group_id = null;
+			try{
+				group_id = fieldNames_g.next();
+			}catch(NoSuchElementException e){
+				break;
+			}
+			//企画団体IDを取得
+
+			while(id_row.get(group_id) >= hoge1){pw.print(","); hoge1++;}
+			//団体IDと列を一致
+
+
+			switch(syurui){
+
+				case "rep" :
+
+					try{		//色々出力
+
+						String temp = root_a.get(root_g.get(group_id).get(syurui).toString()).get(string).toString();
+						temp = remove_comma(temp);
+						pw.print(temp+",");
+
+					}catch(NullPointerException e){
+						pw.print(",");
+					}
+
+					break;
+
+				case "member":
+
+					int i = 0;
+					int member_size = Integer.MIN_VALUE;
+
+					try{
+						member_size = root_g.get(group_id).get(syurui).size();
+					}catch(NullPointerException e){}
+
+					while(member_size > i){
+
+							try{		//色々出力
+
+								String temp = root_a.get(root_g.get(group_id).get(syurui).get(i).toString()).get(string).toString();
+								temp = remove_comma(temp);
+								pw.print(temp+" ");
+
+							}catch(NullPointerException e){}
+
+							i++;
+					}
+
+					pw.print(",");
+
+					break;
+
+			}
+
+
+
+
+			hoge1++;
+		}
+		bw.newLine();
+
+	}
+
+
+
+	private static void output_hanrei(String string, String syurui) {
+
+		switch(syurui){
+		case "rep":
+				pw.print("責任者の"+string);
+				break;
+		case "member":
+				pw.print("副責の"+string);
+				break;
+		}
+	}
+
+
+
+	private static void group2(String string) throws IOException {
+
+		pw.print("企画団体名(カナ),");
+
+	    int hoge1 = 1;
+	    fieldNames_g = root_g.fieldNames();
+
+		while(fieldNames_g.hasNext()){
+
+			String group_id = null;
+
+			try{
+				group_id = fieldNames_g.next();
+			}catch(NoSuchElementException e){
+				break;
+			}
+
+			while(id_row.get(group_id) > hoge1){pw.print(","); hoge1++;}
+
+			try{
+				String temp = root_g.get(group_id).get(string).toString();
+				temp = remove_comma(temp);
+				pw.print(temp+",");	//団体名を出力
+			}catch(NullPointerException e){}
+
+			hoge1++;
+		}
+		bw.newLine();
+
+	}
+
+
+
+	private static void group() throws IOException{
+
+	    pw.print("企画団体名,");
+
+	    int hoge1 = 1;
+	    fieldNames_g = root_g.fieldNames();
+		while(fieldNames_g.hasNext()){
+
+			String temp = null;
+
+			try{
+				temp = fieldNames_g.next();
+			}catch(NoSuchElementException e){
+				break;
+			}
+
+			String temp1 =root_g.get(temp).get("name").toString();
+			temp1=remove_comma(temp1);
+
+			pw.print(temp1+",");	//団体名を出力
+
+			id_row.put(temp, hoge1); //団体IDと列を関連付け
+
+			hoge1++;
+		}
+		bw.newLine();
+	}
+
+
+
+	private static String remove_comma(String temp1) {
+
+		Pattern p = Pattern.compile(",");
+		Matcher m = p.matcher(temp1);
+		String result = m.replaceAll("、");
+
+		return result;
+	}
+
+
+}
+
